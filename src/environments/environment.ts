f34d310c1f1dfdb3b1911c17c3d0413bff5fee0a
danielq7429@gmail.com <danielq7429@gmail.com>
2021-02-22 13:57:20 -0300

    dani 22-02-2021

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   //API_URL: 'http://192.168.0.112:3000'
-  //API_URL: 'http://localhost:3000'
-  API_URL: 'http://192.168.88.14:3000'
+  API_URL: 'http://localhost:3000'
+  //API_URL: 'http://192.168.88.14:3000'
 };
 
 /*
