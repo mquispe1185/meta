845a2786e32c52e2ebd65a04c1f48259ea23531b
matiasq <mquispe1185@gmail.com>
2021-04-28 17:40:40 -0300

    footer fixes

--- src/app/modules/shared/footer-visitantes/footer-visitantes.component.ts
+++ src/app/modules/shared/footer-visitantes/footer-visitantes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { AngularTokenService } from 'angular-token';
 import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
@@ -13,6 +15,8 @@ export class FooterVisitantesComponent implements OnInit {
   public safeURL:SafeResourceUrl;
 
   constructor(private sanitizer:DomSanitizer,
+              public tokenService: AngularTokenService,
+              public router: Router,
               public deviceService: DeviceDetectorService) { }
 
   ngOnInit(): void {
@@ -27,5 +31,27 @@ export class FooterVisitantesComponent implements OnInit {
     }else{
       this.safeURL = this.sanitizer.bypassSecurityTrustResourceUrl('https://web.whatsapp.com/send?phone=543874522407');
     }
+  }
+
+  irAPanel(){
+    if (this.tokenService.currentUserData === null || this.tokenService.currentUserData === undefined){
+      this.login();
+    }else{
+    switch(this.tokenService.currentUserData.rol_id) {
+      case 1: {
+        this.router.navigate(['listacomercios']);
+        break;
+      }
+      case 2: case 3: {
+        this.router.navigate(['comerciopanel']);
+        break;
+      }
+
+   }
+    }
+  }
+
+  login(){
+    this.tokenService.signInOAuth('google');
   }
 }
